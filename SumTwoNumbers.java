5f0895c6c4f19d8996c7f61bca93666000b1fb45
Kyle H. Perez <k.p13@icloud.com>
2024-09-06 15:07:06 -0400

    Update SumTwoNumbers.java
    Updated the class name to match the file name.

--- SumTwoNumbers.java
+++ SumTwoNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class SumTwoNumbers {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
         System.out.println("Enter a number: ");
